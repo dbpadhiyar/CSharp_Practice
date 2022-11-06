55e45792be726cf89bb7f64ca877f75298bf7553
Devendra Padhiyar <dbpadhiyar10@outlook.com>
2022-11-06 18:52:20 +0530

    19_23 (if,if_else,nested if_else,switch,nested_switch) & Marksheet Project

---
+++ 19_23_If_and_Switch_statement/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+public class Program
+{
+    enum dayPart
+    {
+        winter = 1,
+        summer = 2,
+        monsoon = 3
+    }
+
+    public static void Main()
+    {
+        int a, b, c, d, e;
+        a = 10;
+        b = 20;
+        c = 30;
+        d = 40;
+        e = 50;
+
+        Console.WriteLine("--------------- If Loop Start ---------------");
+        if (b > a)
+        {
+            Console.WriteLine("B is Greater Than A");
+        }
+
+        Console.WriteLine("--------------- If Loop End   ---------------\n");
+
+        Console.WriteLine("------------- If Else Loop Start -------------");
+        if (a < b)
+            Console.WriteLine("A is Less Than B");
+        else
+            Console.WriteLine("B is Greater Than A");
+        Console.WriteLine("------------- If Else Loop End   -------------\n");
+
+        Console.WriteLine("---------- Nested If Else Loop Start ----------");
+        if (a > b && a > c)
+            Console.WriteLine("A is Greater Than B And C");
+        else if (b > a && b > c)
+            Console.WriteLine("B is Greater Than A And C");
+        else if (c > a && c > b)
+            Console.WriteLine("C is Greater Than A And B");
+        Console.WriteLine("---------- Nested If Else Loop End   ----------\n");
+
+        Console.WriteLine("-----  Switch & Nested Switch Statement Start  -----");
+
+        int dayOfWeek = 1;
+
+        switch (dayOfWeek)
+        {
+            case 1:
+                Console.WriteLine("It's Monday");
+                switch (dayPart.monsoon)
+                {
+                    case dayPart.winter:
+                        Console.WriteLine("it's Monday of {0}", dayPart.winter);
+                        break;
+                    case dayPart.summer:
+                        Console.WriteLine("it's Monday of {0}", dayPart.summer);
+                        break;
+                    case dayPart.monsoon:
+                        Console.WriteLine("it's Monday of {0}", dayPart.monsoon);
+                        break;
+                }
+                break;
+            case 2:
+                Console.WriteLine("It's Tuesday");
+                break;
+            case 3:
+                Console.WriteLine("It's Wednesday");
+                break;
+            case 4:
+                Console.WriteLine("It's Thursday");
+                break;
+            case 5:
+                Console.WriteLine("It'sss Friday");
+                break;
+            case 6:
+                Console.WriteLine("It'ssssss Saturday");
+                break;
+            case 7:
+                Console.WriteLine("It's Sunday");
+                break;
+            default:
+                Console.WriteLine("Invalid Day");
+                break;
+        }
+        Console.WriteLine("-----  Switch & Nested Switch Statement End    -----\n");
+    }
+}
---
+++ 24_MarksheetProject/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+public class Program
+{
+    static int count = 0;
+    static string  result = string.Empty;
+    public static void Main()
+    {
+        Console.WriteLine("Welcome To MArksheet Maker");
+
+        Console.WriteLine("Please Enter Your Name");
+        string studentName = Console.ReadLine();
+
+        Console.WriteLine("Please Enter Your Standard");
+        string studentStandard = Console.ReadLine();
+
+        Console.WriteLine("Enter Your Maths Mark");
+        int mathsMark = int.Parse(Console.ReadLine());
+        if (mathsMark < 33)
+            count++;
+
+        Console.WriteLine("Enter Your Science Mark");
+        int ScienceMark = int.Parse(Console.ReadLine());
+        if (ScienceMark < 33)
+            count++;
+
+        Console.WriteLine("Enter Your Social Science  Mark");
+        int socialScienceMark = int.Parse(Console.ReadLine());
+        if (socialScienceMark < 33)
+            count++;
+
+        Console.WriteLine("Enter Your Language Mark");
+        int languageMark = int.Parse(Console.ReadLine());
+        if (languageMark < 33)
+            count++;
+
+        Console.WriteLine("Enter Your Sports Mark");
+        int sportsMark = int.Parse(Console.ReadLine());
+        if (languageMark < 33)
+            count++;
+
+        Console.WriteLine("----------------  Marksheet  ----------------\n\n");
+        Console.WriteLine("Student Name : {0}", studentName);
+        Console.WriteLine("Standard : {0} \n", studentStandard);
+        Console.WriteLine("Maths : {0}", mathsMark);
+        Console.WriteLine("Science : {0}", ScienceMark);
+        Console.WriteLine("Social Science : {0}", socialScienceMark);
+        Console.WriteLine("Language : {0}", languageMark);
+        Console.WriteLine("Sports : {0}", sportsMark);
+
+        int totalMark = mathsMark + ScienceMark + socialScienceMark + languageMark + sportsMark;
+        float percentage = totalMark / 5f;
+        if (count > 0)
+            result = $"You Failed in {count} Subject";
+        else
+            result = $"You Passed Exam With {percentage} Percentage";
+        Console.WriteLine(@"Final Result => {0}", result);
+        Console.WriteLine("\n\n");
+        Console.WriteLine("----------------------------------------------");
+    }
+}
